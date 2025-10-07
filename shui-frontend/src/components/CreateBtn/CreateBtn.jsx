a53ce74b2416dc09484afc5dd0d9bd9ec8ef85ea
Embla Holgersson <embla.holgersson@hotmail.com>
2025-10-08 01:36:25 +0200

    fix: fix href
    Removed localhost from url

--- shui-frontend/src/components/CreateBtn/CreateBtn.jsx
+++ shui-frontend/src/components/CreateBtn/CreateBtn.jsx
@@ -3,7 +3,7 @@ import './CreateBtn.css';
 import edit from '../../assets/edit.png';
 function CreateBtn() {
   return (
-    <a className='create-btn' href='http://localhost:5173/create'>
+    <a className='create-btn' href='/create'>
       <img
         className='create-btn__icon'
         src={edit}
